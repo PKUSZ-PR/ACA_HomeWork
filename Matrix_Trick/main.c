e9ee342f5f0dbd84ed1e5794bddea4e24e2bd100
pandigreat <pandigreat@163.com>
2017-11-09 16:08:40 +0800

    Tmp

--- Matrix_Trick/main.c
+++ Matrix_Trick/main.c
@@ -3,10 +3,21 @@
 #include <time.h>
 #include "matrix_alg.h"
 #define K_SIZE 1024
+#define N 2048
+int filesize[] = {128, 256, 512, 1024, 2048};
+int A[N][N], B[N][N], C[N][N];
 
-
-int main()
-{
+int main(){
+    /*Initialization*/
+    srand(time(0));
+    int i, j, k;
+    for(i = 0; i < N, i++){
+        for(j = 0; j < N; j++){
+            A[i][j] = rand() % 5 + 1;
+            B[i][j] = rand() % 5 + 1;
+            C[i][j] = rand() % 5 + 1;
+        }
+    }
 
 
     return 0;
